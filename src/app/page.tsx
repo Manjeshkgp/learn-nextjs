2f7c85da6743a231d6f3a414fe7341e358c4e684
Manjesh Kumar Sharma <manjeshkgp@gmail.com>
2023-03-21 00:44:01 +0530

    Created Important Reusable Components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import styles from './page.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
-
 export default function Home() {
   return (
     <main className='bg-red-400'>Testing</main>
